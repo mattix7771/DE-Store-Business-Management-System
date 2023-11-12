2464b2a19674275987272507774b8876f584f9d4
Mattia <mattione7@gmail.com>
2023-11-12 21:53:32 +0000

    Refactored to use updateRecord() Fully commented

--- PriceControlService/PriceControlService.cs
+++ PriceControlService/PriceControlService.cs
@@ -2,27 +2,36 @@
 
 namespace PriceControlService;
 
+/* PriceControlService is a service that manages the price of products */
 public class PriceControlService
 {
+    // Database variable to communicate with database
     DataAccessLayer.Database db = new DataAccessLayer.Database();
 
-    //get and set product price
+    /// <summary>
+    /// Sets a new price for a product
+    /// </summary>
+    /// <param productName> The name of the product whose price must be changes </param>
+    /// <param productPrice> The new price of the product </param>
     public async Task SetProductPrice(String productName, double productPrice){
         
+        // Get product entry from database
         Task<ProductModel> product = db.GetProduct("name", productName);
 
         product.Result.Price = productPrice;
-
-        await db.DeleteProduct(productName);
-        Console.WriteLine($"Product set: Name = {product.Result.Name}, Price = {product.Result.Price}, Stock = {product.Result.Stock}");
-        await db.SetProduct(product.Result.Name, product.Result.Price, product.Result.Stock);
+        await db.UpdateProduct(productName, "price", productPrice);
     }
 
+    /// <summary>
+    /// Sets a new price for a product
+    /// </summary>
+    /// <param productName> The name of the product to retrieve from database </param>
+    /// <returns> The price of the product </returns>
     public double GetProductPrice(String productName){
 
+        // Get product entry from database
         Task<ProductModel> product = db.GetProduct("name", productName);
 
         return product.Result.Price;
     }
-
 }
